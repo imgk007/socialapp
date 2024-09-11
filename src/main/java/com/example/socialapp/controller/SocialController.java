fe4d95c7cddda00b49431806868c87cf05056ed2
imgk007 <gnanavelkumarang7@gmail.com>
2024-09-12 00:49:38 +0530

    i18n creation

--- src/main/java/com/example/socialapp/controller/SocialController.java
+++ src/main/java/com/example/socialapp/controller/SocialController.java
@@ -4,26 +4,42 @@ import com.example.socialapp.user.User;
 import com.example.socialapp.user.UserDaoService;
 import com.example.socialapp.user.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class SocialController {
 
+    private MessageSource messageSource;
+    public SocialController(MessageSource messageSource){
+        this.messageSource=messageSource;
+    }
     private UserDaoService service;
 
-    public SocialController(UserDaoService service) {
-        this.service=service;
-    }
+    //public SocialController(UserDaoService service) {
+    //    this.service=service;
+    //}
 
     @GetMapping("/hello")
     public String helloWorld () {
         return "Hi, it works GK!!!!!";
     }
+
+    //internationalization-> change of language using header Accept-Language
+    @GetMapping("/helloi18n")
+    public String helloWorldInternationalization () {
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+    }
+
+
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
